acb9e04bbb202faa24c823dcedeeb5003cc81a2d
Minhazul-Arfin <arefinminhaz45@gmail.com>
2024-09-15 15:36:12 +0600

    symfony crud applied, admin panel added

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,15 +11,23 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class ProductController extends AbstractController
 {
+    // DEFAULT ROUTE
+    #[Route('/product', name: 'product')]
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
 
     // // CREATE NEW PRODUCT
     // #[Route('/product', name: 'create_product')]
     // public function createProduct(EntityManagerInterface $entityManager): Response
     // {
     //     $product = new Product();
-    //     $product->setName('Keyboard');
-    //     $product->setPrice(3550);
-    //     $product->setDescription('Ergonomic and stylish!');
+    //     $product->setName('Windows PC');
+    //     $product->setPrice(200000);
+    //     $product->setDescription('Core i9 with extraordinary hardwear pack');
 
     //     // tell Doctrine you want to (eventually) save the Product (no queries yet)
     //     $entityManager->persist($product);
@@ -33,43 +41,43 @@ class ProductController extends AbstractController
 
     // FETCH DATA FROM DATABASE
     // WAY 1
-    #[Route('/product/{id}', name: 'product_show')]
-    public function show(EntityManagerInterface $entityManager, int $id): Response
-    {
-        $product = $entityManager->getRepository(Product::class)->find($id);
+    // #[Route('/product/{id}', name: 'product_show')]
+    // public function show(EntityManagerInterface $entityManager, int $id): Response
+    // {
+    //     $product = $entityManager->getRepository(Product::class)->find($id);
 
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
+    //     if (!$product) {
+    //         throw $this->createNotFoundException(
+    //             'No product found for id '.$id
+    //         );
+    //     }
 
-        return new Response('Check out this great product: '.$product->getName());
+    //     return new Response('Check out this great product: '.$product->getName()." with price of ".$product->getPrice()." have speciality of: ".$product->getdescription());
 
-        // or render a template
-        // in the template, print things with {{ product.name }}
-        // return $this->render('product/show.html.twig', ['product' => $product]);
-    }
+    //     // or render a template
+    //     // in the template, print things with {{ product.name }}
+    //     // return $this->render('product/show.html.twig', ['product' => $product]);
+    // }
 
 
 
     // UPDATE DATA
-    #[Route('/product/edit/{id}', name: 'product_edit')]
-    public function update(EntityManagerInterface $entityManager, int $id): Response
-    {
-        $product = $entityManager->getRepository(Product::class)->find($id);
+    // #[Route('/product/edit/{id}', name: 'product_edit')]
+    // public function update(EntityManagerInterface $entityManager, int $id): Response
+    // {
+    //     $product = $entityManager->getRepository(Product::class)->find($id);
 
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
+    //     if (!$product) {
+    //         throw $this->createNotFoundException(
+    //             'No product found for id '.$id
+    //         );
+    //     }
 
-        $product->setName('APPLE PC');
-        $entityManager->flush();
+    //     $product->setName('APPLE PC');
+    //     $entityManager->flush();
 
-        return $this->redirectToRoute('product_show', [
-            'id' => $product->getId()
-        ]);
-    }
+    //     return $this->redirectToRoute('product_show', [
+    //         'id' => $product->getId()
+    //     ]);
+    // }
 }
